088ddd60af39e33ce4f3e9128cea93ed63d3cc4f
Javier Villagomez <javier_diaz16@hotmail.com>
2021-09-08 00:50:47 -0500

    Creacion de paquetes y modulos

---
+++ MyMainPackage/some_main_script.py
@@ -0,0 +1,2 @@
+def main_report():
+    print('Hey Im a function inside /MyMainPackage/some_main_script.py')
---
+++ myprogram.py
@@ -0,0 +1,5 @@
+#Este es un programa cualquiera, la intencion es utilizar una funcion dentro de un modulo.
+
+from mymodule import my_func
+
+my_func()
